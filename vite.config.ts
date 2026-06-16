30832ef9e70ff4218ea2a0f3f6b8c716f760b02f
halvesm <horacio.moura@prof.ce.gov.br>
2026-06-16 10:50:58 -0300

    Trigger Vercel deploy test with correct git credentials config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
+// Trigger deployment check with halvesm config
+
+
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
